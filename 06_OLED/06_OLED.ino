f431e1224f5f880ffc40fb7b2c153e036f9dd0ba
vongomben <davide.gomba@gmail.com>
2020-07-16 12:21:26 +0200

    updating

--- 06_OLED/06_OLED.ino
+++ 06_OLED/06_OLED.ino
@@ -8,10 +8,6 @@
 #include <Wire.h>
 #endif
 
-#include "Ultrasonic.h"
-
-Ultrasonic ultrasonic(0);
-
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 void setup(void) {
@@ -21,23 +17,12 @@ void setup(void) {
 
 void loop(void) {
 
-  long RangeInInches;
-  long RangeInCentimeters;
 
-  RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
-  Serial.println(RangeInCentimeters);//0~400cm
 
-  char cast = char(RangeInCentimeters);
 
   u8g2.clearBuffer();                   // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
   u8g2.drawStr(0, 10, "Hello World!");  // write something to the internal memory
-  u8g2.setCursor(0, 40);
-  u8g2.print(cast);
-  //u8g2.drawStr(0, 20, cast);  // write something to the internal memory
-
-
   u8g2.sendBuffer();                    // transfer internal memory to the display
   delay(100);
-  Serial.println(RangeInCentimeters);
 }
--- 07_Distance/07_Distance.ino
+++ 07_Distance/07_Distance.ino
@@ -1,23 +1,31 @@
 /*
 
   Example taken from Seeestudio Wiki
-
+  for the Grove Ultrasonic Ranger
+  https://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/
   adapted to work with Arduino Grove Carrier (on MKR1010)
   
   https://www.arduino.cc/en/Guide/MKRConnectorCarrier
   by Davide Gomba
 
+  Library here 
+  https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger/archive/master.zip
+
+  Install Library Sketch>include Library> Add Zip Library
 */
 
 
 
 
 #include "Ultrasonic.h"
- 
+
+int led = 5;
+
 Ultrasonic ultrasonic(0);
 void setup()
 {
     Serial.begin(9600);
+    pinMode(led, OUTPUT);
 }
 void loop()
 {
@@ -31,7 +39,12 @@ void loop()
     delay(250);
  */
     RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
-    Serial.println(RangeInCentimeters);//0~400cm
-  //  Serial.println(" cm");
+    Serial.print(RangeInCentimeters);//0~400cm
+    //Serial.println(" cm");
+    int value = map(RangeInCentimeters, 10, 210, 255, 0);
+    Serial.print(" - ");
+    Serial.println(value);
+    analogWrite(led, value);
+    
     delay(25);
 }
