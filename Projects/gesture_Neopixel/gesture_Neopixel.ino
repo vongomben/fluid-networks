09fcd94d83bcb164a7b2c34dc15eeefa5c0317fc
vongomben <davide.gomba@gmail.com>
2020-07-17 17:40:28 +0200

    updating readm and gesture to neopixel

--- Projects/gesture_Neopixel/gesture_Neopixel.ino
+++ Projects/gesture_Neopixel/gesture_Neopixel.ino
@@ -10,9 +10,24 @@ int bottomLeds[] =  {12, 11, 10,  9,  8,  7,  6, 5, 4};
 int leftLeds[] =    {0,  15, 14, 13, 12, 11, 10, 9, 8};
 int rightLeds[] =   {0,   1,  2,  3,  4,  5,  6, 7, 8};
 
+// gesture sensor stuff
+
+
+#include <Wire.h>
+#include "paj7620.h"
+
+
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
 
+
+/*
+  Notice: When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
+        You also can adjust the reaction time according to the actual circumstance.
+*/
+#define GES_REACTION_TIME       500             // You can adjust the reaction time according to the actual circumstance.
+#define GES_ENTRY_TIME          800             // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
+#define GES_QUIT_TIME           1000
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -26,9 +41,139 @@ void setup() {
   strip.begin();
   strip.setBrightness(255);
   strip.show(); // Initialize all pixels to 'off'
+
+  uint8_t error = 0;
+
+  Serial.begin(9600);
+  Serial.println("\nPAJ7620U2 TEST DEMO: Recognize 9 gestures.");
+
+  error = paj7620Init();          // initialize Paj7620 registers
+  if (error)
+  {
+    Serial.print("INIT ERROR,CODE:");
+    Serial.println(error);
+  }
+  else
+  {
+    Serial.println("INIT OK");
+  }
+  Serial.println("Please input your gestures:\n");
+
+
 }
 
 void loop() {
+
+  uint8_t data = 0, data1 = 0, error;
+
+  error = paj7620ReadReg(0x43, 1, &data);             // Read Bank_0_Reg_0x43/0x44 for gesture result.
+  if (!error)
+  {
+    switch (data)                                   // When different gestures be detected, the variable 'data' will be set to different values by paj7620ReadReg(0x43, 1, &data).
+    {
+      case GES_RIGHT_FLAG:
+        delay(GES_ENTRY_TIME);
+        paj7620ReadReg(0x43, 1, &data);
+        if (data == GES_FORWARD_FLAG)
+        {
+          Serial.println("Forward");
+          delay(GES_QUIT_TIME);
+        }
+        else if (data == GES_BACKWARD_FLAG)
+        {
+          Serial.println("Backward");
+          delay(GES_QUIT_TIME);
+        }
+        else
+        {
+          Serial.println("Right");
+          directions(strip.Color(255, 0, 0), 50, "right");
+        }
+        break;
+      case GES_LEFT_FLAG:
+        delay(GES_ENTRY_TIME);
+        paj7620ReadReg(0x43, 1, &data);
+        if (data == GES_FORWARD_FLAG)
+        {
+          Serial.println("Forward");
+          delay(GES_QUIT_TIME);
+        }
+        else if (data == GES_BACKWARD_FLAG)
+        {
+          Serial.println("Backward");
+          delay(GES_QUIT_TIME);
+        }
+        else
+        {
+          Serial.println("Left");
+          directions(strip.Color(255, 0, 0), 50, "left");
+        }
+        break;
+      case GES_UP_FLAG:
+        delay(GES_ENTRY_TIME);
+        paj7620ReadReg(0x43, 1, &data);
+        if (data == GES_FORWARD_FLAG)
+        {
+          Serial.println("Forward");
+          delay(GES_QUIT_TIME);
+        }
+        else if (data == GES_BACKWARD_FLAG)
+        {
+          Serial.println("Backward");
+          delay(GES_QUIT_TIME);
+        }
+        else
+        {
+          Serial.println("Up");
+          directions(strip.Color(255, 0, 0), 50, "up");
+        }
+        break;
+      case GES_DOWN_FLAG:
+        delay(GES_ENTRY_TIME);
+        paj7620ReadReg(0x43, 1, &data);
+        if (data == GES_FORWARD_FLAG)
+        {
+          Serial.println("Forward");
+          delay(GES_QUIT_TIME);
+        }
+        else if (data == GES_BACKWARD_FLAG)
+        {
+          Serial.println("Backward");
+          delay(GES_QUIT_TIME);
+        }
+        else
+        {
+          Serial.println("Down");
+          directions(strip.Color(255, 0, 0), 50, "down");
+        }
+        break;
+      case GES_FORWARD_FLAG:
+        Serial.println("Forward");
+        delay(GES_QUIT_TIME);
+        break;
+      case GES_BACKWARD_FLAG:
+        Serial.println("Backward");
+        delay(GES_QUIT_TIME);
+        break;
+      case GES_CLOCKWISE_FLAG:
+        Serial.println("Clockwise");
+        break;
+      case GES_COUNT_CLOCKWISE_FLAG:
+        Serial.println("anti-clockwise");
+        break;
+      default:
+        paj7620ReadReg(0x44, 1, &data1);
+        if (data1 == GES_WAVE_FLAG)
+        {
+          Serial.println("wave");
+        }
+        break;
+    }
+  }
+  delay(100);
+
+
+
   /* // Some example procedures showing how to display to the pixels:
     colorWipe(strip.Color(255, 0, 0), 50); // Red
     colorWipe(strip.Color(0, 255, 0), 50); // Green
@@ -45,10 +190,6 @@ void loop() {
 
   */
 
-  directions(strip.Color(255, 0, 0), 50, "up");
-  directions(strip.Color(255, 0, 0), 50, "down");
-  directions(strip.Color(255, 0, 0), 50, "left");
-  directions(strip.Color(255, 0, 0), 50, "right");
 }
 
 // Fill the dots one after the other with a color
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ official repository and documentation for the Fluid Networks Workshop
 |   3 |                     Micro USB cable                     |      CONN      |   |
 |   4 |                  [Grove - Red LED Button](https://wiki.seeedstudio.com/Grove-LED_Button/)                 | INPUT / OUTPUT |   |
 |   5 |                [Grove - OLED Display 0.96"](https://wiki.seeedstudio.com/Grove-OLED-Display-0.96-SSD1315/)               |     DISPLAY    |   |
-|   6 |                   [Grove - RGB LED Ring](https://wiki.seeedstudio.com/Grove%20-%20RGB%20LED%20Ring%20%2820%20-%20WS2813%20Mini%29/)                  |     OUTPUT     |   |
+|   6 |                   [Grove - RGB LED Ring](https://wiki.seeedstudio.com/Grove%20-%20RGB%20LED%20Ring%20%2820%20-%20WS2813%20Mini%29/)                  |     OUTPUT     |  [neopixel](https://github.com/adafruit/adafruit_NeoPixel)  |
 |   7 |            [Grove - Ultrasonic Distance Sensor](https://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/)           |      INPUT     |  [ultrasonic.h](https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger/archive/master.zip) |
 |   8 |    [Grove - Light Sensor v1.2 - LS06-S phototransistor](https://wiki.seeedstudio.com/Grove-Light_Sensor/)   |      INPUT     |   |
 |   9 |      [Grove - Temperature & Humidity Sensor (DHT11)](https://wiki.seeedstudio.com/Grove-TemperatureAndHumidity_Sensor/)      |      INPUT     |   |
