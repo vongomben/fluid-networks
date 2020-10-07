f96e0fb76e0e560e615e4f6b4b4d05198eb514d6
vongomben <davide.gomba@gmail.com>
2020-10-07 12:25:07 +0200

    added DHT11

---
+++ 11_DHT/11_DHT.ino
@@ -0,0 +1,65 @@
+
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+
+#include "DHT.h"
+#include <Wire.h>
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+/*Notice: The DHT10 is different from other DHT* sensor ,it uses i2c interface rather than one wire*/
+/*So it doesn't require a pin.*/
+
+//#define DHTTYPE DHT10
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#if defined(ARDUINO_ARCH_AVR)
+    #define debug  Serial
+
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+    #define debug  SerialUSB
+#else
+    #define debug  Serial
+#endif
+
+void setup() {
+
+    debug.begin(115200);
+    debug.println("DHTxx test!");
+    Wire.begin();
+
+    dht.begin();
+}
+
+void loop() {
+    float temp_hum_val[2] = {0};
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+
+    if (!dht.readTempAndHumidity(temp_hum_val)) {
+        debug.print("Humidity: ");
+        debug.print(temp_hum_val[0]);
+        debug.print(" %\t");
+        debug.print("Temperature: ");
+        debug.print(temp_hum_val[1]);
+        debug.println(" *C");
+    } else {
+        debug.println("Failed to get temprature and humidity value.");
+    }
+
+    delay(1500);
+}
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ official repository and documentation for the Fluid Networks Workshop
 |   6 |                   [Grove - RGB LED Ring](https://wiki.seeedstudio.com/Grove%20-%20RGB%20LED%20Ring%20%2820%20-%20WS2813%20Mini%29/)                  |     OUTPUT     |  [neopixel](https://github.com/adafruit/adafruit_NeoPixel)  |
 |   7 |            [Grove - Ultrasonic Distance Sensor](https://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/)           |      INPUT     |  [ultrasonic.h](https://github.com/Seeed-Studio/Seeed_Arduino_UltrasonicRanger/archive/master.zip) |
 |   8 |    [Grove - Light Sensor v1.2 - LS06-S phototransistor](https://wiki.seeedstudio.com/Grove-Light_Sensor/)   |      INPUT     |   |
-|   9 |      [Grove - Temperature & Humidity Sensor (DHT11)](https://wiki.seeedstudio.com/Grove-TemperatureAndHumidity_Sensor/)      |      INPUT     |   |
+|   9 |      [Grove - Temperature & Humidity Sensor (DHT11)](https://wiki.seeedstudio.com/Grove-TemperatureAndHumidity_Sensor/)      |      INPUT     | [Grove DHT Library](https://github.com/Seeed-Studio/Grove_Temperature_And_Humidity_Sensor/archive/master.zip)  |
 |  10 |                       [Grove - NFC](https://wiki.seeedstudio.com/Grove_NFC/)                       |      INPUT     |   |
 |  11 |                      [Grove - Servo](https://wiki.seeedstudio.com/Grove-Servo/)                      |     OUTPUT     |   |
 |  12 |               [Grove - 2-Channel SPDT Relay](https://wiki.seeedstudio.com/Grove-2-Channel_SPDT_Relay/)              |     OUTPUT     |   |
