86a6cff1de583c6e8da8c2e320f5db8dbad5180d
vongomben <davide.gomba@gmail.com>
2020-12-09 16:53:08 +0100

    Update 06_OLED.ino

--- 06_OLED/06_OLED.ino
+++ 06_OLED/06_OLED.ino
@@ -8,24 +8,24 @@
 #include <Wire.h>
 #endif
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0,
+    /* clock=*/ SCL,
+    /* data=*/ SDA,
+    /* reset=*/ U8X8_PIN_NONE);
 
-long count = 0;
+long int count = 0;
 
 void setup(void) {
   Serial.begin(9600);
   u8g2.begin();
-  u8g2.enableUTF8Print();
+  u8g2.setDisplayRotation(U8G2_R2);          // rotations --> https://github.com/olikraus/u8g2/wiki/u8g2reference#setdisplayrotation
+  u8g2.setFont(u8g2_font_fub11_tf);         // here --> https://github.com/olikraus/u8g2/wiki/fntlistall
+  //  u8g2.enableUTF8Print();
+  u8g2.drawStr(10, 40, "Fluid Networks");   // here --> https://github.com/olikraus/u8g2/wiki/u8g2reference#drawstr
+ u8g2.sendBuffer();                         // transfer internal memory to the display
+
 }
 
 void loop(void) {
 
-  count = count + 1;
-  
-  Serial.println(count);
-  u8g2.clearBuffer();                   // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  u8g2.print(0, 10, count);  // write something to the internal memory
-  u8g2.sendBuffer();                    // transfer internal memory to the display
-  delay(100);
 }
