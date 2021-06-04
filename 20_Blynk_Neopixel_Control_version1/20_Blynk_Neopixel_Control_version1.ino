75d7d2a9e6ecf133c78c58982d1c44f8ef4d513a
vongomben <davide.gomba@gmail.com>
2021-06-04 11:42:30 +0200

    Create 20_Blynk_Neopixel_Control_version1.ino
    adding Blynk Example

---
+++ 20_Blynk_Neopixel_Control_version1/20_Blynk_Neopixel_Control_version1.ino
@@ -0,0 +1,114 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example shows how to use Arduino MKR 1010
+  to connect your project to Blynk.
+
+  Note: This requires WiFiNINA library
+    from http://librarymanager/all#WiFiNINA
+
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
+
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <BlynkSimpleWiFiNINA.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "your-blynk-token";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "your-wifi-ssid";
+char pass[] = "your-wifi-pasw";
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define LED_PIN    3
+#define LED_COUNT 16
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE(V0) {
+
+  int r = param[0].asInt();
+  int g = param[1].asInt();
+  int b = param[2].asInt();
+
+  colorWipe(strip.Color(r,   g,   b), 15); // Red
+
+
+  Serial.print(r);
+  Serial.print(" - ");
+  Serial.print(g);
+  Serial.print(" - ");
+  Serial.print(b);
+  Serial.print(" - ");
+
+
+
+
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
