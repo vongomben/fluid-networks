ffdc8f8928e43d1db0d5d3afa1efe87a4914b393
vongomben <davide.gomba@gmail.com>
2021-12-08 22:32:37 +0100

    Adding Minimal Json Examples

--- 19_3-sensors-Json-over-MQTT/19_3-sensors-Json-over-MQTT.ino
+++ 19_3-sensors-Json-over-MQTT/19_3-sensors-Json-over-MQTT.ino
@@ -41,25 +41,16 @@ unsigned long lastMillis = 0;
 
 
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
 
 #include "Ultrasonic.h"
 
-#define DHTPIN 2
-#define DHTTYPE DHT11
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
 
 int val;
 const int sensore = A0;
 Ultrasonic ultrasonic(0);
 
-uint32_t delayMS;
 
-float temp;
-float hum;
 
 void connect() {
   Serial.print("checking wifi...");
@@ -101,40 +92,6 @@ void setup() {
   client.onMessage(messageReceived);
   connect();
 
-
-  // Initialize device.
-  dht.begin();
-
-
-
-  Serial.println(F("DHTxx Unified Sensor Example"));
-  // Print temperature sensor details.
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
-
-
-
 }
 
 void loop() {
@@ -142,29 +99,6 @@ void loop() {
   client.loop();
   if (!client.connected()) {
     connect();
-  }
-
-
-  // Get temperature event and print its value.
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    delay(delayMS);
-    temp = event.temperature;
-
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    delay(delayMS);
-    hum = event.relative_humidity;
-
   }
 
 
@@ -180,8 +114,6 @@ void loop() {
 
   myObject["Brightness"] = light;
   myObject["Distance"] = RangeInCentimeters;
-  myObject["Temperature"] = temp;
-  myObject["Humidity"] = hum;
 
   // JSON.stringify(myVar) can be used to convert the json var to a String
   String jsonString = JSON.stringify(myObject);
